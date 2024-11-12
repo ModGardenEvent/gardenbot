d7725169766934e0562168a1ed9e43d9db1e0ed9
MerchantPug <81167232+MerchantPug@users.noreply.github.com>
2024-11-12 14:43:44 +1100

    Minor refactor.

--- src/events/ready.ts
+++ src/events/ready.ts
@@ -1,7 +1,6 @@
 import { bot } from '../bot.js'
 
 bot.events.ready = async ({ user, shardId }) => {
-    if (shardId === bot.gateway.lastShardId) {
+    if (shardId === bot.gateway.lastShardId)
         bot.logger.info(`Successfully connected to the gateway as ${user.username}`)
-    }
 }
