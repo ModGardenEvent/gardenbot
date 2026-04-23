13b4439721c538ef6a53700ee0fe3a802f76548a
Anastasia Chrysanthemum <cow@chrysanthemum.lgbt>
2026-04-23 11:12:25 +1000

    refactor: Simplify runEachHour method.

--- src/main/java/net/modgarden/gardenbot/util/TimeUtil.java
+++ src/main/java/net/modgarden/gardenbot/util/TimeUtil.java
@@ -1,25 +1,18 @@
 package net.modgarden.gardenbot.util;
 
-import java.util.Calendar;
-import java.util.TimeZone;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 
 public class TimeUtil {
-	public static final long DAY_MS = 86400000;
+	public static final long HOUR_MS = 3600000;
+	public static final long DAY_MS = HOUR_MS * 24;
 	public static final long WEEK_MS = DAY_MS * 7;
 
 	public static void runEachHour(Runnable runnable) {
 		long now = System.currentTimeMillis();
-		Calendar time = Calendar.getInstance(TimeZone.getTimeZone("UTC"));
 
-		time.set(Calendar.HOUR, time.get(Calendar.HOUR) + 1);
-		time.set(Calendar.MINUTE, 0);
-		time.set(Calendar.SECOND, 0);
-		time.set(Calendar.MILLISECOND, 0);
-
-		long etaMs = time.getTimeInMillis() - now;
+		long etaMs = HOUR_MS - now % HOUR_MS;
 
 		try (ScheduledExecutorService service = Executors.newSingleThreadScheduledExecutor()) {
 			service.scheduleAtFixedRate(runnable, etaMs, 1, TimeUnit.HOURS);
