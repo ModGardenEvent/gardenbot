a172acf88acf858886060c1fd9f49555e088d44a
Anastasia Chrysanthemum <cow@chrysanthemum.lgbt>
2026-05-25 07:19:51 +1000

    style: TeamInviteUtil.

--- src/main/java/net/modgarden/gardenbot/util/TeamInviteUtil.java
+++ src/main/java/net/modgarden/gardenbot/util/TeamInviteUtil.java
@@ -3,22 +3,18 @@ package net.modgarden.gardenbot.util;
 import net.modgarden.gardenbot.GardenBot;
 import net.modgarden.gardenbot.database.DatabaseAccess;
 
-import javax.xml.crypto.Data;
 import java.sql.SQLException;
 
 public class TeamInviteUtil {
 	public static void revokeExpiredInvitesEachHour() {
-		TimeUtil.runEachHour(() -> {
-				DatabaseAccess.bind().run(() -> {
-					try {
-						DatabaseAccess db = DatabaseAccess.get();
-						int updated = db.revokeInvalidTeamInvites();
-						GardenBot.LOG.info("Successfully revoked {} expired team invites.", updated);
-					} catch (SQLException e) {
-						GardenBot.LOG.error("Failed to revoke expired invites. ", e);
-					}
-				});
-
-		});
+		TimeUtil.runEachHour(() -> DatabaseAccess.bind().run(() -> {
+			try {
+				DatabaseAccess db = DatabaseAccess.get();
+				int updated = db.revokeInvalidTeamInvites();
+				GardenBot.LOG.info("Successfully revoked {} expired team invites.", updated);
+			} catch (SQLException e) {
+				GardenBot.LOG.error("Failed to revoke expired invites. ", e);
+			}
+		}));
 	}
 }
